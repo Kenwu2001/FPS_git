9a45e02a2096ed4cdeae008aa5f12d0a0d6a2dd0
s109000114 <imeukg2001@gmail.com>
2024-02-03 22:26:32 +0800

    clone version 2019

---
+++ Assets/Scripts/BulletScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    public float FlyingSpeed = 80;
+    public float LifeTime = 5;
+
+    public void InitAndShoot(Vector3 Direction)
+    {
+        Rigidbody rigidbody = this.GetComponent<Rigidbody>();
+
+        rigidbody.velocity = Direction * FlyingSpeed;
+
+        Invoke("KillYourself", LifeTime);
+    }
+
+    public void KillYourself()
+    {
+        GameObject.Destroy(this.gameObject);
+    }
+
+    public float damageValue = 15;
+
+    void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.SendMessage("Hit", damageValue);
+        KillYourself();
+    }
+}
