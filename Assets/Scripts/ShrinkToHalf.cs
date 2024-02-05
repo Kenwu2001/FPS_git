aa526ae7b898895041ce08f5ceef51269103b362
s109000114 <imeukg2001@gmail.com>
2024-02-05 19:37:10 +0800

    Safe zone shrimps for 4 times.

--- Assets/Scripts/ShrinkToHalf.cs
+++ Assets/Scripts/ShrinkToHalf.cs
@@ -14,12 +14,31 @@ public class ShrinkToHalf : MonoBehaviour
 
     void Update()
     {
+        float time = Time.time;
 
-        // Debug.Log("Current length of the object: " + transform.localScale.x);
-        if (transform.localScale.x > originalScale.x / 4)
+        if (time >= 5 && time < 10 && transform.localScale.x > originalScale.x * 0.75)
         {
-            // 如果物件的大小大於原始大小的一半，則縮小物件
             transform.localScale -= new Vector3(shrinkSpeed, shrinkSpeed, shrinkSpeed) * Time.deltaTime;
         }
+        else if (time >= 10 && time < 15 && transform.localScale.x > originalScale.x * 0.5)
+        {
+            transform.localScale -= new Vector3(shrinkSpeed, shrinkSpeed, shrinkSpeed) * Time.deltaTime;
+        }
+        else if (time >= 15 && time < 20 && transform.localScale.x > originalScale.x * 25)
+        {
+            transform.localScale -= new Vector3(shrinkSpeed, shrinkSpeed, shrinkSpeed) * Time.deltaTime;
+        }
+        else if (time >= 20 && transform.localScale.x > originalScale.x * 0.15)
+        {
+            transform.localScale -= new Vector3(shrinkSpeed, shrinkSpeed, shrinkSpeed) * Time.deltaTime;
+        }
+
+
+        // Debug.Log("Current length of the object: " + transform.localScale.x);
+        // if (transform.localScale.x > originalScale.x / 4)
+        // {
+        //     // 如果物件的大小大於原始大小的一半，則縮小物件
+        //     transform.localScale -= new Vector3(shrinkSpeed, shrinkSpeed, shrinkSpeed) * Time.deltaTime;
+        // }
     }
 }
